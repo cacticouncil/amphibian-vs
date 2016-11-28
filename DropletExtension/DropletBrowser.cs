a29f3af5f1fb9548441e39e87998813400c51730
Jackson Yelinek <jackyelinek@gmail.com>
2016-11-28 11:43:43 -0500

    Initial code commit

---
+++ DropletExtension/DropletBrowser.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser;
+using DotNetBrowser.WinForms;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Web;
+using DotNetBrowser.DOM;
+using DotNetBrowser.DOM.Events;
+
+namespace DropletExtension
+{
+    public partial class DropletBrowser : Form
+    {
+        public Browser chromeBrowser;
+
+        public string result;
+
+        public System.Diagnostics.Process server;
+
+        public DropletBrowser()
+        {
+            InitializeComponent();
+
+            this.Activated += DropletBrowser_Activated;
+            this.Deactivate += DropletBrowser_Deactivate;
+
+            InitializePythonServer();
+
+            InitializeDotNetBrowser();
+        }
+
+        private void DropletBrowser_Deactivate(object sender, EventArgs e)
+        {
+            Droplet.Instance.dropletEditorActive = false;
+        }
+
+        private void DropletBrowser_Activated(object sender, EventArgs e)
+        {
+            Droplet.Instance.dropletEditorActive = true;
+        }
+
+        public void InitializeDotNetBrowser()
+        {
+            chromeBrowser = BrowserFactory.Create();
+            BrowserView browserView = new WinFormsBrowserView(chromeBrowser);
+            browserView.Browser.FinishLoadingFrameEvent += Browser_FinishLoadingFrameEvent;
+            Controls.Add((Control)browserView);
+            chromeBrowser.LoadURL("http://localhost:8888/Resources/Droplet/example/example.html");
+        }
+
+        private void Browser_FinishLoadingFrameEvent(object sender, DotNetBrowser.Events.FinishLoadingEventArgs e)
+        {
+            if (e.IsMainFrame)
+            {
+                Browser myBrowser = e.Browser;
+                DOMDocument document = chromeBrowser.GetDocument();
+
+                // I need to find the correct element and the right events to use in order to notice when text is changed on droplet
+                DOMElement tmp = document.GetElementByClassName("droplet-wrapper-div");
+                DOMElement keyChange = document.GetElementByClassName("droplet-wrapper-div");
+                DOMElement dragCover = document.GetElementByClassName("droplet-drag-cover");
+                DOMElement mouseUpTextEditor = document.GetElementByClassName("ace_content");
+                DOMElement keyPressTextEditor = document.GetElementByClassName("ace_text-input");
+
+                dragCover.AddEventListener(DOMEventType.OnMouseUp, DomEventHandlerOnMouseUp, true);
+                //dragCover.AddEventListener(DOMEventType.OnMouseMove, DomEventHandlerOnMouseUp, true);
+                //dragCover.AddEventListener(DOMEventType.OnMouseOver, DomEventHandlerOnMouseUp, true);
+                mouseUpTextEditor.AddEventListener(DOMEventType.OnMouseUp, DomEventHandlerOnMouseUp, true);
+                keyPressTextEditor.AddEventListener(DOMEventType.OnKeyDown, DomEventHandlerOnMouseUp, true);
+                keyChange.AddEventListener(DOMEventType.OnKeyPress, DomEventHandlerOnMouseUp, true);
+                tmp.AddEventListener(DOMEventType.OnMouseOver, DomEventHandlerOnMouseUp, true);
+            }
+        }
+
+        private void DomEventHandlerOnMouseUp(object sender, DOMEventArgs e)
+        {
+            //Droplet.Instance.hasUpdated = true;
+            VisualStudioTextEditor.SetVSText();
+        }
+
+        public void InitializePythonServer()
+        {
+            server = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+            //this should be set to hidden, but the process doesn't close properly if it is set to hidden
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C python -m http.server 8888";
+            server.StartInfo = startInfo;
+            server.Start();
+        }
+
+        public string GetText()
+        {
+            string script = @"(function() 
+                {
+                return this.editor.getValue();
+                })(); ";
+
+            var tmp = chromeBrowser.ExecuteJavaScriptAndReturnValue(script);
+            if (!tmp.IsNull())
+            {
+                result = tmp.GetString();
+            }
+
+            // for testing purposes only
+            //using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\Users\jacky\Desktop\Testing Stuff\TestFromDroplet.txt"))
+            //{
+            //    sw.Write(result);
+            //}
+
+            return result;
+        }
+
+        public void SetText(string code)
+        {
+            string script = "this.editor.setValue(`" + code + "`);";
+
+            chromeBrowser.ExecuteJavaScript(script);
+        }
+    }
+}
---
+++ DropletExtension/VisualStudioTextEditor.cs
@@ -0,0 +1,139 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="VisualStudioTextEditor.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System.IO;
+using System.Windows.Forms;
+using DotNetBrowser.DOM.Events;
+
+namespace DropletExtension
+{
+    internal sealed class VisualStudioTextEditor
+    {
+        /// <summary>
+        /// The layer of the adornment.
+        /// </summary>
+        private readonly IAdornmentLayer layer;
+
+        /// <summary>
+        /// Text view where the adornment is created.
+        /// </summary>
+        private readonly IWpfTextView view;
+
+        private DTE dte;
+
+        private static string activeWindowFilePath;
+
+        bool eventsMade = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VisualStudioTextEditor"/> class.
+        /// </summary>
+        /// <param name="view">Text view to create the adornment for</param>
+        public VisualStudioTextEditor(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            layer = view.GetAdornmentLayer("VisualStudioTextEditor");
+
+            this.view = view;
+            this.view.LayoutChanged += OnLayoutChanged;
+
+            dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+            dte.Events.WindowEvents.WindowActivated += OnWindowActivated;
+        }
+
+        public static void SetVSText()
+        {
+            if (Droplet.Instance == null)
+            {
+                return;
+            }
+            if (Droplet.Instance.dropletEditorActive == true)
+            {
+                string dropletText = Droplet.Instance.dropletBrowser.GetText();
+                if (dropletText != "")
+                {
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(activeWindowFilePath))
+                        {
+                            sw.Write(dropletText);
+                        }
+                    }
+                    catch
+                    {
+                        // good programming right here (not really)
+                    }
+                }
+                //Droplet.Instance.hasUpdated = false;
+            }
+        }
+
+        private void OnWindowActivated(Window GotFocus, Window LostFocus)
+        {
+            
+            if (null != GotFocus.Document)
+            {
+                Document curDoc = GotFocus.Document;
+                activeWindowFilePath = curDoc.FullName;
+                if (Droplet.Instance == null)
+                {
+                    return;
+                }
+
+                string tmpFilePath = view.TextBuffer.Properties.GetProperty<ITextDocument>(typeof(ITextDocument)).FilePath;
+
+                if (Droplet.Instance.dropletEditorActive == false && string.Compare(activeWindowFilePath, tmpFilePath, true) == 0)
+                {
+                    Droplet.Instance.dropletBrowser.SetText(view.TextBuffer.CurrentSnapshot.GetText());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
+        /// </summary>
+        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
+        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
+        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
+        /// </remarks>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            
+            if (activeWindowFilePath == null)
+            {
+                return;
+            }
+            if (Droplet.Instance == null)
+            {
+                return;
+            }
+            if (e.OldSnapshot.GetText() == e.NewSnapshot.GetText())
+            {
+                return;
+            }
+            if (Droplet.Instance.dropletEditorActive == false)
+            {
+                Droplet.Instance.dropletBrowser.SetText(e.NewViewState.EditSnapshot.GetText());
+            }
+
+        }
+
+    }
+}
